20b0986d9ad235adec3b5c3d9a2c0e010e6dfd5f
Programmer Fresh Group 2 <fgmprog2@gmail.com>
2023-03-13 14:06:05 +0700

    menambahkan module untuk frontend dan inisialisai tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./src/app.js", "./src/pages/*.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
